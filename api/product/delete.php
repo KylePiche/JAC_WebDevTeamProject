822f4770229acfaa7d0a76481590d60593004daa
Greg Barre <g.barre@hotmail.ca>
2022-06-29 16:10:37 -0400

    create api CRUD for Product

--- api/product/delete.php
+++ api/product/delete.php
@@ -7,6 +7,25 @@
 
     // Includes
     include_once '../../connection-config.php';
+    include_once '../../models/Product.php';
 
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
 
+    // Instantiate Product object
+    $prod = new Product($db);
+
+    // Get raw data
+    $data = json_decode(file_get_contents("php://input"));
+
+    // Set ID
+    $prod->id = $data->id;
+
+    // Delete Product
+    if($prod->delete()) {
+        echo json_encode(array('message' => 'Product Deleted'));
+    } else {
+        echo json_encode(array('message' => 'Product Not Deleted'));
+    }
 ?>
