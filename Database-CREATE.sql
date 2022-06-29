c286498e62f3bda889570b3977539cfeee747c40
Greg Barre <g.barre@hotmail.ca>
2022-06-29 12:11:33 -0400

    added spec details and finalized design

--- Database-CREATE.sql
+++ Database-CREATE.sql
@@ -21,49 +21,54 @@ CREATE TABLE products (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	desc varchar(500) DEFAULT NULL,
-	type enum('laptop', 'desktop', 'tablet'), /*potentially more/less categories*/
+	type enum('laptop', 'desktop'),
 	price decimal(10,2) NOT NULL,
 	CPUid int NOT NULL,
 	GPUid int NOT NULL,
 	memoryID int NOT NULL,
 	storageID int NOT NULL,
-	screenID int NOT NULL, /*probably better to have an id for 'no screen' in the lookup table rather than using NULL here*/
+	screenID int NOT NULL,
 	PRIMARY KEY(id)
 );
 
 USE computerstore;
 CREATE TABLE spec_GPU (
 	id int NOT NULL AUTO_INCREMENT,
-	desc varchar(255) NOT NULL, /*should maybe use a numeric type for some specs, so that values can be compared in search filters etc..*/ 
-								/*should have an option here for 'no discrete GPU' or similar*/
+	desc varchar(255) NOT NULL,
+	memory int DEFAULT NULL,
 	PRIMARY KEY(id)
 );
 
 USE computerstore;
 CREATE TABLE spec_CPU (
 	id int NOT NULL AUTO_INCREMENT,
-	desc varchar(255) NOT NULL, /*should maybe use a numeric type for some specs, so that values can be compared in search filters etc..*/
+	desc varchar(255) NOT NULL,
+	clockSpeed decimal(3,1) NOT NULL,
+	coreCount int NOT NULL,
 	PRIMARY KEY(id)
 );
 
 USE computerstore;
 CREATE TABLE spec_memory (
 	id int NOT NULL AUTO_INCREMENT,
-	desc varchar(255) NOT NULL, /*should maybe use a numeric type for some specs, so that values can be compared in search filters etc..*/
+	desc varchar(255) NOT NULL,
+	memory int NOT NULL,
 	PRIMARY KEY(id)
 );
 
 USE computerstore;
 CREATE TABLE spec_storage (
 	id int NOT NULL AUTO_INCREMENT,
-	desc varchar(255) NOT NULL, /*should maybe use a numeric type for some specs, so that values can be compared in search filters etc..*/
+	desc varchar(255) NOT NULL,
+	capacity int NOT NULL,
 	PRIMARY KEY(id)
 );
 
 USE computerstore;
 CREATE TABLE spec_screen (
 	id int NOT NULL AUTO_INCREMENT,
-	desc varchar(255) NOT NULL, /*should maybe use a numeric type for some specs, so that values can be compared in search filters etc..*/
+	desc varchar(255) NOT NULL,
+	size decimal(3,1) DEFAULT NULL,
 	PRIMARY KEY(id)
 );
 
@@ -71,7 +76,7 @@ USE computerstore;
 CREATE TABLE orders (
 	id int NOT NULL AUTO_INCREMENT,
 	userID int NOT NULL,
-	status enum('in cart', 'pending', 'purchased'), /*not sure about this column as far as implementation and values that we will need*/
+	status enum('in cart','purchased'),
 	PRIMARY KEY(id)
 );
 
@@ -85,9 +90,17 @@ CREATE TABLE order_details (
 );
 
 USE computerstore;
-CREATE TABLE favorites (
+CREATE TABLE favorite_lists (
 	id int NOT NULL AUTO_INCREMENT,
 	userID int NOT NULL,
+	dateCreated datetime DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id)
+)
+
+USE computerstore;
+CREATE TABLE favorites (
+	id int NOT NULL AUTO_INCREMENT,
+	listID int NOT NULL,
 	productID int NOT NULL,
 	dateAdded datetime DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(id)
@@ -107,5 +120,7 @@ ALTER TABLE orders ADD CONSTRAINT FK_Orders_Users FOREIGN KEY (userID) REFERENCE
 ALTER TABLE order_details ADD CONSTRAINT FK_OrderDetails_Products FOREIGN KEY (productID) REFERENCES products(id);
 ALTER TABLE order_details ADD CONSTRAINT FK_OrderDetails_Orders FOREIGN KEY (orderID) REFERENCES orders(id);
 
-ALTER TABLE favorites ADD CONSTRAINT FK_Favorites_Users FOREIGN KEY (userID) REFERENCES users(id);
+ALTER TABLE favorite_lists ADD CONSTRAINT FK_FavoriteLists_Users FOREIGN KEY (userID) REFERENCES users(id);
+
+ALTER TABLE favorites ADD CONSTRAINT FK_Favorites_FavoriteLists FOREIGN KEY (listID) REFERENCES favorite_lists(id);
 ALTER TABLE favorites ADD CONSTRAINT FK_Favorites_Products FOREIGN KEY (productID) REFERENCES products(id);
