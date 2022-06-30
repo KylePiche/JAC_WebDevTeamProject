7cb0e8ef44d20c73ac9f41a07b24aaf55872c997
Greg Barre <g.barre@hotmail.ca>
2022-06-30 10:23:59 -0400

    added "Integrated Graphics" entry to the GPU inserts.  Added 1 to the GPUid fields in products to compensate

--- Database-INSERT.sql
+++ Database-INSERT.sql
@@ -1,12 +1,13 @@
 /*INSERT INTO table1 (field1, field2) VALUES (value1, value2);*/
 use computerstore;
 INSERT INTO products (name, desc, type, price, imageUrl, CPUid, GPUid, memoryID, storageID, screenID) VALUES
-("Ultimus Gaming PC", "Dominate the playing field with this top of the line gaming PC.", "desktop", "2000.99", "placeholderURL", 1, 1, 1, 1, 1),
-("Ultimus Gaming Laptop", "Dominate the playing field on the go with this top of the line gaming laptop.", "laptop", "2200.99", "placeholderURL", 2, 2, 2, 2, 2),
-("Ultimus Work Laptop", "Dominate the office space with this top of the line workstation laptop.", "laptop", "1800.99", "placeholderURL", 3, 3, 3, 3, 3);
+("Ultimus Gaming PC", "Dominate the playing field with this top of the line gaming PC.", "desktop", "2000.99", "placeholderURL", 1, 2, 1, 1, 1),
+("Ultimus Gaming Laptop", "Dominate the playing field on the go with this top of the line gaming laptop.", "laptop", "2200.99", "placeholderURL", 2, 3, 2, 2, 2),
+("Ultimus Work Laptop", "Dominate the office space with this top of the line workstation laptop.", "laptop", "1800.99", "placeholderURL", 3, 4, 3, 3, 3);
 
 
 INSERT INTO spec_GPU (desc, memory) VALUES
+ ("Integrated Graphics", null),
  ("RTX 3090Ti", 16),
  ("RTX 2080M", 10),
  ("Aoreus GPU 2495", 5);
