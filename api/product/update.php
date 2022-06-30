907ee3f22bc60d216c3ad1afc627bff0075c3a5a
Greg Barre <g.barre@hotmail.ca>
2022-06-30 13:07:32 -0400

    cleaned up file

--- api/product/update.php
+++ api/product/update.php
@@ -18,7 +18,7 @@
 
     // Get raw data
     $data = json_decode(file_get_contents("php://input"));
-    echo json_encode($data);
+
     // Set properties
     $prod->id = $data->id;
     $prod->name = $data->name;
@@ -37,24 +37,6 @@
     $prod->storage_capacity = $data->storage_capacity;
     $prod->screen_desc = $data->screen_desc;
     $prod->screen_size = $data->screen_size;
-
-    // $prod->id = $data['id'];
-    // $prod->name = $data['name'];
-    // $prod->desc = $data['desc'];
-    // $prod->type = $data['type'];
-    // $prod->price = $data['price'];
-    // $prod->imageUrl = $data['imageUrl'];
-    // $prod->cpu_desc = $data['cpu_desc'];
-    // $prod->cpu_speed = $data['cpu_speed'];
-    // $prod->cpu_cores = $data['cpu_cores'];
-    // $prod->gpu_desc = $data['gpu_desc'];
-    // $prod->gpu_memory = $data['gpu_memory'];
-    // $prod->memory_desc = $data['memory_desc'];
-    // $prod->ram = $data['ram'];
-    // $prod->storage_desc = $data['storage_desc'];
-    // $prod->storage_capacity = $data['storage_capacity'];
-    // $prod->screen_desc = $data['screen_desc'];
-    // $prod->screen_size = $data['screen_size'];
 
     // Update Product
     if($prod->update()) {
