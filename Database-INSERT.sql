639dd1f769d97f89469a7d7b8637c29958712c91
Kyle Piché <kylepiche@gmail.com>
2022-06-30 10:32:16 -0400

    Update Database-INSERT.sql

--- Database-INSERT.sql
+++ Database-INSERT.sql
@@ -1,9 +1,6 @@
 /*INSERT INTO table1 (field1, field2) VALUES (value1, value2);*/
 use computerstore;
-INSERT INTO products (name, desc, type, price, imageUrl, CPUid, GPUid, memoryID, storageID, screenID) VALUES
-("Ultimus Gaming PC", "Dominate the playing field with this top of the line gaming PC.", "desktop", "2000.99", "placeholderURL", 1, 2, 1, 1, 1),
-("Ultimus Gaming Laptop", "Dominate the playing field on the go with this top of the line gaming laptop.", "laptop", "2200.99", "placeholderURL", 2, 3, 2, 2, 2),
-("Ultimus Work Laptop", "Dominate the office space with this top of the line workstation laptop.", "laptop", "1800.99", "placeholderURL", 3, 4, 3, 3, 3);
+
 
 
 INSERT INTO spec_GPU (desc, memory) VALUES
@@ -31,3 +28,8 @@ INSERT INTO spec_screen (desc, size) VALUES
  ("No Screen", null),
  ("15 inch display 1080p", 15),
  ("10 inch 1440p display", 10);
+ 
+ INSERT INTO products (name, desc, type, price, imageUrl, CPUid, GPUid, memoryID, storageID, screenID) VALUES
+("Ultimus Gaming PC", "Dominate the playing field with this top of the line gaming PC.", "desktop", "2000.99", "placeholderURL", 1, 2, 1, 1, 1),
+("Ultimus Gaming Laptop", "Dominate the playing field on the go with this top of the line gaming laptop.", "laptop", "2200.99", "placeholderURL", 2, 3, 2, 2, 2),
+("Ultimus Work Laptop", "Dominate the office space with this top of the line workstation laptop.", "laptop", "1800.99", "placeholderURL", 3, 4, 3, 3, 3);
