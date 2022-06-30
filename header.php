e414ea601f73bed9077467d3dbcd65bd3fbf3c90
Kyle Piché <kylepiche@gmail.com>
2022-06-29 20:29:05 -0400

    Login added, and header is dynamic

--- header.php
+++ header.php
@@ -2,39 +2,46 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container">
 
-      <img src="./assets/img/logo.png" alt="Logo">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <img src="./assets/img/logo.png" alt="Logo">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#laptop">Shop</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="signup.php">Sign up</a>
-          </li>
-          <li class="nav-item">
-            <i class="bi bi-cart"></i>
-            <i class="bi bi-person"></i>
-          </li>
+        <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#laptop">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <?php if (!isset($_SESSION['email'])) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">Sign up</a>
+                    </li>
+                <?php } else { ?>
+                    <li id="nav-item">
+                        <a href="logout.php" class="nav-link">Log out</a>
+                    </li>
+                    <li class="nav-item">
+                        <i class="bi bi-cart"></i>
+                        <i class="bi bi-person"></i>
+                    </li>
+                <?php } ?>
 
-        </ul>
 
-      </div>
+
+            </ul>
+
+        </div>
     </div>
-  </nav>
+</nav>
---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+include_once 'connection-config.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!--cdn bootstrap 5.1.x-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!--cdn bootstrap icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <?php require 'header.php';?>
+    <div class="container">
+                <div class="row">
+                    <div class="col-xs-6 col-xs-offset-3">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h3>LOGIN</h3>
+                            </div>
+                            <div class="panel-body">
+                                <p>Login to make a purchase.</p>
+                                <form method="post" action="login_submit.php">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="password" placeholder="Password(min. 8 characters)" pattern=".{8,}">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" value="Login" class="btn btn-primary">
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="panel-footer">Don't have an account yet? <a href="signup.php">Register</a></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+</body>
+
+</html>
