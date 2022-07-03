6c1499dea7388cc69e682258bc1c86628d1d9145
Kyle Piché <kylepiche@gmail.com>
2022-07-02 23:55:41 -0400

    Initial Product Details page

--- productDetails.php
+++ productDetails.php
@@ -1,8 +1,19 @@
 <?php
 include_once 'connection-config.php';
 session_start();
-$sql = "SELECT * FROM products";
+// if (!isset($_GET['id'])) {
+//     header('Location: products.php');
+// }
+$sql = "SELECT * FROM products WHERE id = {$_GET['id']}";
+// var_dump($sql);
+
 $sqlResult = mysqli_query($mysqli, $sql);
+// var_dump($sqlResult);
+$row = $sqlResult->fetch_assoc();
+var_dump($row);
+if ($row == NULL) {
+    header('Location: products.php');
+}
 $count = 1;
 ?>
 <!DOCTYPE html>
@@ -12,7 +23,7 @@ $count = 1;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Our products</title>
+    <title>Test</title>
     <!--cdn bootstrap 5.1.x-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -24,22 +35,21 @@ $count = 1;
 
 <body>
     <?php require 'header.php'; ?>
-    <div class="container mt-5">
-        <div class="card-group">
-            <?php while ($row = mysqli_fetch_array($sqlResult)) { ?>
-                <div class="card m-2" style="width: 18rem;">
-                    <img class="card-img-top" src="<?php echo $row['imageUrl'] ?>" alt="Promo image of <?php echo $row['name'] ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $row['name'] ?></h5>
-                        <p class="card-text"><?php echo $row['desc'] ?></p>
-                        <a href="productDetails.php?id=<?php echo $row['id'] ?>" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            <?php } ?>
+    <div class="container">
+
+        <div class="title">
+            <h1><?php echo $row['name'] ?></h1>
         </div>
+        <div class="row">
+            <div class="col"><img src="<?php echo $row['imageUrl'] ?>" alt="Image of <?php echo $row['name'] ?>"></div>
+            <div class="col">
+                <p><?php echo $row['desc'] ?></p>
+            </div>
+
+        </div>
+
     </div>
 
-    <!-- </div> -->
 </body>
 
 </html>
